b8234e45244c00f4397dca9fd91426a08753e393
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-04-09 23:05:07 -0300

    First Commit

---
+++ com.commons.booking/src/main/java/com/commons/booking/model/mapper/BookingMapper.java
@@ -0,0 +1,21 @@
+package com.commons.booking.model.mapper;
+
+import com.commons.booking.model.Booking;
+import com.commons.booking.model.DTO.BookingDTO;
+import com.commons.booking.model.DTO.BookingResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BookingMapper {
+
+    BookingMapper INSTANCE = Mappers.getMapper(BookingMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    Booking bookingDTOToBooking(BookingDTO bookingDTO);
+
+    BookingResponse bookingToBookingResponse (Booking booking);
+
+
+}
